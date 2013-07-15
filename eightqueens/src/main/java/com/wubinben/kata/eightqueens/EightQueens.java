19237fdd897df2e8d05c4c8c1058d221cb509aa9
Ben Wu <wubin28@gmail.com>
2013-07-15 22:50:35 +0800

    Refactoring: finished algorithm in enumerate().

--- eightqueens/src/main/java/com/wubinben/kata/eightqueens/EightQueens.java
+++ eightqueens/src/main/java/com/wubinben/kata/eightqueens/EightQueens.java
@@ -9,6 +9,7 @@ import java.util.List;
  * Time: 下午9:45
  */
 public class EightQueens {
+
   public static List<int[]> getSolutions(int numberOfQueens) {
     List<int[]> solutions = new ArrayList<int[]>();
     enumerate(numberOfQueens, solutions);
@@ -16,6 +17,41 @@ public class EightQueens {
   }
 
   private static void enumerate(int numberOfQueens, List<int[]> solutions) {
-    //To change body of created methods use File | Settings | File Templates.
+    int[] array = new int[numberOfQueens];
+    enumerate(array, 0, solutions);
   }
+
+  private static void enumerate(int[] array, int rowNumber, List<int[]> solutions) {
+    int ARRAY_LENGTH = array.length;
+    if (isLastQueenLocatedProperly(rowNumber, ARRAY_LENGTH)) {
+      solutions.add(array.clone());
+    } else {
+      for (int i = 0; i < ARRAY_LENGTH; i++) {
+        putQueenOnRowNumberToAColumnNumber(array, rowNumber, i);
+        if (isConsistent(array, rowNumber)) enumerate(array, rowNumber + 1, solutions);
+      }
+    }
+  }
+
+  /**
+   * Return true if queen placement array[rowNumber] does not conflict with
+   * other queens array[0] through array[rowNumber-1]
+   */
+  private static boolean isConsistent(int[] array, int rowNumber) {
+    for (int i = 0; i < rowNumber; i++) {
+      if (array[i] == array[rowNumber])             return false;   // same column
+      if ((array[i] - array[rowNumber]) == (rowNumber - i)) return false;   // same major diagonal
+      if ((array[rowNumber] - array[i]) == (rowNumber - i)) return false;   // same minor diagonal
+    }
+    return true;
+  }
+
+  private static void putQueenOnRowNumberToAColumnNumber(int[] array, int rowNumber, int i) {
+    array[rowNumber] = i;
+  }
+
+  private static boolean isLastQueenLocatedProperly(int rowNumber, int ARRAY_LENGTH) {
+    return rowNumber == ARRAY_LENGTH;
+  }
+
 }
